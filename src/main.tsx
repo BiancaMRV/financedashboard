04b75e49839529a9df593987a9e2c689260131e0
Bianca Vilaverde <biancamargarida2014@gmail.com>
2025-04-14 22:58:29 +0100

    some minor changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+// import "./index.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Transactions from "./pages/Transactions";
+import Investments from "./pages/Investments";
+import Crypto from "./pages/Crypto";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Dashboard />} />
+        <Route path="transactions" element={<Transactions />} />
+        <Route path="investments" element={<Investments />} />
+        <Route path="crypto" element={<Crypto />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
